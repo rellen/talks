552cbe0c39471477de50416b1ff54f3df18b0175
Robert Ellen <robert@alembic.com.au>
2024-07-23 03:10:51 +1000

    Elixir Types 1.17 wip

---
+++ 2024-07-elixir-1-17-types/one-17/shell.nix
@@ -0,0 +1,29 @@
+{ pkgs ? import <nixpkgs> { }, nixpkgs ? <nixpkgs> }:
+let
+    inherit (pkgs.lib) optional optionals;
+    erlang = pkgs.beam.interpreters.erlang_27;
+    elixir = pkgs.beam.packages.erlang_27.elixir_1_17;
+
+in pkgs.mkShell rec {
+  name = "1.17 R27";
+  buildInputs = with pkgs; [
+    rebar
+    rebar3
+    erlang
+    elixir
+    elixir_ls
+    ] ++ optional stdenv.isLinux inotify-tools ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+
+shellHook = ''
+     # this allows mix to work on the local directory
+     mkdir -p .nix-mix
+     mkdir -p .nix-hex
+     export MIX_HOME=$PWD/.nix-mix
+     export HEX_HOME=$PWD/.nix-hex
+     export PATH=$MIX_HOME/bin:$PATH
+     export PATH=$HEX_HOME/bin:$PATH
+     export LANG=en_US.UTF-8
+     export ERL_AFLAGS="-kernel shell_history enabled"
+     export ERL_LIBS=""
+   '';
+}
