f72306f07c89ea06ad741cfc8a38872e91f04fa4
Robert Ellen <robert@alembic.com.au>
2025-03-09 15:36:25 +1000

    initial git/nix setup

---
+++ 2025-03-hakyll/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "prenterm";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+        inherit (pkgs.lib) optional optionals;
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+
+      in
+      {
+
+        formatter = treefmtEval.config.build.wrapper;
+
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs =
+              [
+                presenterm
+              ]
+              ++ optional stdenv.isLinux inotify-tools
+              ++ optionals stdenv.isDarwin (
+                with darwin.apple_sdk.frameworks;
+                [
+                  CoreFoundation
+                  CoreServices
+                  Cocoa
+                ]
+              );
+          };
+
+      }
+    );
+}
